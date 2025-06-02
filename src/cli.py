963dfde489594033c077c4d777a64d35015ceb5a
Jordan <twinjordann@gmail.com>
2025-06-02 04:38:09 -0400

    Major update to cli.py

--- src/cli.py
+++ src/cli.py
@@ -4,6 +4,8 @@
 import argparse
 from cve_engine import fetch_cve_data, extract_service, attack_vector_from_string, service_map
 
+home_net = "$HOME_NET"   #please make sure you map your home net on your snort config
+
 def main():
     parser = argparse.ArgumentParser(
         description="ZeroCVEs CLI - Generate Snort rules from CVE IDs with severity scoring"
@@ -22,12 +24,26 @@ def main():
         print(f"No data found for {cve_id}")
         return
 
-    services = extract_service(description, service_map)
+    services = extract_seervice(description, service_map)
     attack_vector = attack_vector_from_string(cvss_vector)
 
-    service_str = ", ".join(s[0] for s in services) if services != ["ANY"] else "ANY"
+    if services == "ANY":
+        service_str = "ANY"
+        dst_port = "any"
 
-    snort_rule = f'alert tcp any any -> any any (msg:"Potential {cve_id} Exploit - {service_str}"; sid=1000001; rev=1;)'
+    else:
+        service_str = ", ".join(s[0] for s in services)
+        dst_port = "any"
+        for _, service_info in services:
+            port = service_info.get("port")
+            if port:
+                dst_port = port
+                break
+         
+    
+    
+
+    snort_rule = f'alert tcp any any -> {home_net} {dst_port} (msg:"Potential {cve_id} Exploit - {service_str}"; sid=1000001; rev=1;)'  #going to use random radiant to pull diffrent sid and revs soon 
 
     print(f"\nCVE: {cve_id}")
     print(f"Attack Vector: {attack_vector} (CVSS: {cvss_vector})")
